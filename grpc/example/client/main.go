bc9c410398a4a6213dcdd0d38db035685b2ab13f
yumaojun03 <719118794@qq.com>
2017-08-02 17:18:31 +0800

    add grpc lb

---
+++ grpc/example/client/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	pb "golang/grpc/example/pb"
+	grpclb "golang/grpc/service"
+	"strconv"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+var (
+	serv = flag.String("service", "hello_service", "service name")
+	reg  = flag.String("reg", "http://192.168.204.7:2379", "register etcd address")
+)
+
+func main() {
+	flag.Parse()
+	r := grpclb.NewResolver(*serv)
+	b := grpc.RoundRobin(r)
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	conn, err := grpc.DialContext(ctx, *reg, grpc.WithInsecure(), grpc.WithBalancer(b))
+	if err != nil {
+		panic(err)
+	}
+
+	ticker := time.NewTicker(1 * time.Second)
+	for t := range ticker.C {
+		client := pb.NewGreeterClient(conn)
+		resp, err := client.SayHello(context.Background(), &pb.HelloRequest{Name: "world " + strconv.Itoa(t.Second())})
+		if err == nil {
+			fmt.Printf("%v: Reply is %s\n", t, resp.Message)
+		}
+	}
+}
